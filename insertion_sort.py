524c77f0b22efb8e1fc3bb24afc5f4e3e729616e
Abner Freitas <defreitasabner@gmail.com>
2022-09-14 20:40:47 -0300

    Insertion Sort implementado com base no livro.

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,13 @@
 from typing import Any, List
 
 def insertion_sort(lista: List[Any]):
-    for scanIndex in range(1, len(lista)): 
-        temp = lista[scanIndex] 
-        minIndex = scanIndex
-        while minIndex > 0 and temp < data[minIndex - 1]: 
-            lista[minIndex] = lista[minIndex - 1] 
-            minIndex -= 1
-            lista[minIndex] = temp
-        print(lista)
     
+    # Percorre toda a lista do início ao fim
+    for indice_atual in range(1, len(lista)): 
+        menor_valor = lista[indice_atual] 
+        indice_menor_valor = indice_atual
+        
+        while indice_menor_valor > 0 and menor_valor < lista[indice_menor_valor - 1]: 
+            lista[indice_menor_valor] = lista[indice_menor_valor - 1] 
+            indice_menor_valor -= 1
+            lista[indice_menor_valor] = menor_valor
