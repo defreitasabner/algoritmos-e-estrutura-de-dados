78e891e6a682f7410a11be857d0cd5df57e7f6a9
Abner Freitas <defreitasabner@gmail.com>
2022-09-14 20:20:04 -0300

    Implementada Insertion Sort usando o exemplo do livro 'Algoritmos para Leigos'.

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,11 +1,12 @@
-data = [94, 71, 0, 62, 48, 80]
+from typing import Any, List
 
-for scanIndex in range(1, len(data)): 
-    temp = data[scanIndex] 
-    minIndex = scanIndex
-    while minIndex > 0 and temp < data[minIndex - 1]: 
-        data[minIndex] = data[minIndex - 1] 
-        minIndex -= 1
-        data[minIndex] = temp
-    print(data)
+def insertion_sort(lista: List[Any]):
+    for scanIndex in range(1, len(lista)): 
+        temp = lista[scanIndex] 
+        minIndex = scanIndex
+        while minIndex > 0 and temp < data[minIndex - 1]: 
+            lista[minIndex] = lista[minIndex - 1] 
+            minIndex -= 1
+            lista[minIndex] = temp
+        print(lista)
     
