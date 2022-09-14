df0eccb0590d89f8037c6bb84775d506fdb44027
Abner Freitas <defreitasabner@gmail.com>
2022-09-14 20:43:24 -0300

    Adicionados alguns comentários na função insertion_sort().

--- insertion_sort.py
+++ insertion_sort.py
@@ -7,6 +7,8 @@ def insertion_sort(lista: List[Any]):
         menor_valor = lista[indice_atual] 
         indice_menor_valor = indice_atual
         
+        # Caso o índice do menor valor seja maior que zero e
+        # o menor valor seja menor que o valor anterior a ele, substitui os valores
         while indice_menor_valor > 0 and menor_valor < lista[indice_menor_valor - 1]: 
             lista[indice_menor_valor] = lista[indice_menor_valor - 1] 
             indice_menor_valor -= 1
