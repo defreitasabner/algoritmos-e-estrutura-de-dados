bc674c31f7e18669d974be5499962a3004af28a2
Abner Freitas <defreitasabner@gmail.com>
2023-06-03 20:08:39 -0300

    Alterada função que gera lista de exemplo.

--- python/utils/gera_lista_exemplo.py
+++ python/utils/gera_lista_exemplo.py
@@ -2,16 +2,4 @@ import random
 from typing import List
 
 def gera_lista_exemplo(tamanho_lista: int) -> List[int]:
-    
-    lista_exemplo = []
-
-    for i in range(tamanho_lista):
-        valor_aleatorio = int(((random.random() * 99) % 100))
-        if valor_aleatorio in lista_exemplo:
-            while valor_aleatorio in lista_exemplo:
-                valor_aleatorio = int(((random.random() * 99) % 100))
-        lista_exemplo.append(valor_aleatorio)
-
-    print(f'Lista de exemplo gerada: {lista_exemplo}')
-
-    return lista_exemplo
+    return random.sample(range(0, tamanho_lista), tamanho_lista)
